df1b2999fc0feb4c39c526af773c1952bbe4a892
Luka Cabraja <luka@cabraja.eu>
2021-05-24 09:53:29 +0200

    predavanje07 partial

---
+++ Predavanje07/src/hr/algebra/errors/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.algebra.errors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author lcabraja
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        
+        while (true) {            
+            list.add("Milica");
+        }
+    }
+}
